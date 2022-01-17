158c67923d545a802a163c953de2dce3c5dcc974
L0rdSaiyan <89421533+L0rdSaiyan@users.noreply.github.com>
2022-01-17 18:05:21 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+public class Main
+{
+    public static void main(String[] args) {
+  
+    LocalDate data = LocalDate.now();
+    
+    System.out.println(data);
+
+  }
+}
